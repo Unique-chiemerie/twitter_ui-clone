ddbd3cbfb31de23b245592f45534fd501d35ec3d
Unique-chiemerie <lilanimate1@gmail.com>
2023-07-12 21:51:34 +0100

    twitter ui building

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,76 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    theme: ThemeData(),
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
     home: mainScreen(),
   ));
+}
+
+class mainScreen extends StatefulWidget {
+  const mainScreen({super.key});
+
+  @override
+  State<mainScreen> createState() => _mainScreenState();
+}
+
+class _mainScreenState extends State<mainScreen>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _tabController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          Container(
+            height: 40,
+            width: 40,
+            color: Colors.blue,
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          TabBar(
+            controller: _tabController,
+            tabs: const [
+              Tab(text: 'For you'),
+              Tab(text: 'For you'),
+            ],
+          ),
+          Expanded(
+            child: TabBarView(
+              controller: _tabController,
+              children: [
+                Container(
+                  height: 100,
+                  width: 100,
+                  margin: const EdgeInsets.all(100),
+                  color: Colors.green,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  margin: const EdgeInsets.all(100),
+                  color: Colors.green,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
